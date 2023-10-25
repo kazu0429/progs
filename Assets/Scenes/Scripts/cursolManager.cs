701df6aadfa0ffc93ff60d662416cad9500dc93c
Akimoto-IT0412 <116004457+Akimoto-IT0412@users.noreply.github.com>
2023-10-26 08:52:03 +0900

    add cursolManager

---
+++ Assets/Scenes/Scripts/cursolManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursolManager : MonoBehaviour
+{
+    Vector3 mousePos, worldPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //マウス座標の取得
+        mousePos = Input.mousePosition;
+        //スクリーン座標をワールド座標に変換
+        worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, 4f, 10f));
+        // X 座標を制限
+        worldPos.x = Mathf.Clamp(worldPos.x, -3f, 3f);
+        worldPos.y = Mathf.Clamp(worldPos.y, 4f, 4f); // ワールド座標のy軸がなぜか-5になるため
+        //ワールド座標を自身の座標に設定
+        transform.position = worldPos;
+    }
+}
