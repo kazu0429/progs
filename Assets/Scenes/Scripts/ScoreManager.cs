7551a3dececb09792a81981c89c1b81ed97e0f90
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-10-27 15:17:16 +0900

    add ScoreMananger

---
+++ Assets/Scenes/Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI ScoreText;
+    public int Score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreText.text = "SCORE\n" + Score.ToString();
+    }
+
+    public void ScoreChanger(int score)
+    {
+        Score += score;
+        ScoreText.text = "SCORE\n" + Score.ToString();
+    }
+}
