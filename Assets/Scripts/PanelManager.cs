a1ae24d71e699600ff6cbb397e22968633a67c16
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-10-30 03:37:21 +0900

    add start & retry

--- Assets/Scripts/PanelManager.cs
+++ Assets/Scripts/PanelManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PanelManager : MonoBehaviour
 {
     //インスペクターウィンドウからゲームオブジェクトを設定する
     [SerializeField] GameObject startPanel;
     [SerializeField] GameObject overPanel;
+
+    public static string gameStatus;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,11 +25,24 @@ public class PanelManager : MonoBehaviour
 
     public void GameStart()
     {
+        gameStatus = "Play";
         startPanel.SetActive(false);
+    }
+
+    public void GameRetry()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GameOver()
+    {
+        gameStatus = "Over";
+        overPanel.SetActive(true);
     }
 
     public void InitToMenu()
     {
+        gameStatus = "startable";
         startPanel.SetActive(true);
         overPanel.SetActive(false);
     }
