1c8199efd3dbb410b9a8e3b111df81c382773291
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-11-11 23:11:23 +0900

    update score result

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI ScoreText;
+    [SerializeField] private TextMeshProUGUI ResultScoreText;
     public int Score = 0;
     // Start is called before the first frame update
     void Start()
@@ -17,5 +18,6 @@ public class ScoreManager : MonoBehaviour
     {
         Score += score;
         ScoreText.text = "SCORE\n" + Score.ToString();
+        ResultScoreText.text = Score.ToString();
     }
 }
